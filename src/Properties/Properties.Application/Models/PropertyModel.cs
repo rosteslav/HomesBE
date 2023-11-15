4cf4faa5ea727a9b11dc40e5e1c2c66c0a71649c
Кris Ivanov <65026817+ChrisIvanov@users.noreply.github.com>
2023-11-15 10:43:27 +0200

    Update PropertyModel.cs

--- src/Properties/Properties.Application/Models/PropertyModel.cs
+++ src/Properties/Properties.Application/Models/PropertyModel.cs
@@ -1,9 +1,38 @@
-﻿namespace BuildingMarket.Properties.Application.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace BuildingMarket.Properties.Application.Models
 {
     public class PropertyModel
     {
-        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
 
-        public string BrokerId { get; set; }
+        [Required]
+        public short NumberOfRooms { get; set; }
+
+        [Required]
+        public string District { get; set; }
+
+        [Required]
+        public float Space { get; set; }
+
+        [Required]
+        public short Floor { get; set; }
+
+        [Required]
+        public short TotalFloorsInBuilding { get; set; }
+
+        public String? BrokerId { get; set; }
+
+        public override String ToString()
+        {
+            return $"Type:{Type},\n" +
+                $"Number Of Rooms:{NumberOfRooms},\n" +
+                $"District:{District},\n" +
+                $"Space:{Space},\n" +
+                $"Floor{Floor},\n" +
+                $"Total Floors In Building:{TotalFloorsInBuilding},\n" +
+                $"Broker Id: {(BrokerId == null ? BrokerId : "No broker added yet")}";
+        }
     }
 }

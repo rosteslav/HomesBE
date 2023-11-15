2b617165046c40824fca00bb78464fd68f488915
Кris Ivanov <65026817+ChrisIvanov@users.noreply.github.com>
2023-11-15 10:44:19 +0200

    Update Property Entity

--- src/Properties/Properties.Domain/Entities/Property.cs
+++ src/Properties/Properties.Domain/Entities/Property.cs
@@ -1,10 +1,32 @@
-﻿namespace BuildingMarket.Properties.Domain.Entities
+using System.ComponentModel.DataAnnotations;
+
+namespace BuildingMarket.Properties.Domain.Entities
 {
     public class Property
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public short NumberOfRooms { get; set; }
+
+        [Required]
+        public string District { get; set; }
+
+        [Required]
+        public float Space { get; set; }
+
+        [Required]
+        public short Floor { get; set; }
+
+        [Required]
+        public short TotalFloorsInBuilding { get; set; }
+
+        [Required]
         public string SellerId { get; set; }
-        public string BrokerId { get; set; }
+
+        public string? BrokerId { get; set; }
     }
 }

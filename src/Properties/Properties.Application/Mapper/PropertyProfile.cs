5330d910550e82eb875ba6ce327f3c4a1535ffee
Petar Belchev <petar.belchev@outlook.com>
2023-11-22 08:10:37 +0200

    Wrongly reverted mapping config was returned.

--- src/Properties/Properties.Application/Mapper/PropertyProfile.cs
+++ src/Properties/Properties.Application/Mapper/PropertyProfile.cs
@@ -12,12 +12,13 @@ namespace BuildingMarket.Properties.Application.Mapper
             CreateMap<AddPropertyInputModel, Property>();
 
             CreateMap<Property, PropertyModel>()
+                .ForMember(x => x.CreatedOnLocalTime, opt => opt.MapFrom(src => src.CreatedOnUtcTime.ToLocalTime()))
                 .ReverseMap()
                 .ForPath(src => src.CreatedOnUtcTime, opt => opt.MapFrom(x => x.CreatedOnLocalTime.ToUniversalTime()));
 
             CreateMap<Property, AddPropertyCommand>()
-               .ForMember(x => x.Model, opt => opt.MapFrom(src => src))
-               .ReverseMap();
+                .ForMember(x => x.Model, opt => opt.MapFrom(src => src))
+                .ReverseMap();
 
             CreateMap<Property, GetAllPropertiesOutputModel>()
                 .ForMember(x => x.Details, opt => opt.MapFrom(src => string.Join(',', src.BuildingType, src.Finish, src.Furnishment, src.Heating)))

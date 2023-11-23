b3339bab955733e2f50bb972aa0b7531eae59992
Kristian Ivanov <kristian.ivanov92@gmail.com>
2023-11-23 16:31:45 +0200

    Last fixes

--- src/Properties/Properties.Application/Models/PropertyModel.cs
+++ src/Properties/Properties.Application/Models/PropertyModel.cs
@@ -2,11 +2,7 @@ namespace BuildingMarket.Properties.Application.Models
 {
     public class PropertyModel : AddPropertyInputModel
     {
-        public required string Type { get; set; }
-
         public required PropertyOptionsModel PropertyOptions { get; set; }
-
-        public string BrokerId { get; set; }
 
         public DateTime CreatedOnLocalTime { get; set; }
     }

0cec6bfa5da30a7ba7df320be9b416b40fc14166
Kristian Ivanov <kristian.ivanov92@gmail.com>
2023-11-27 18:28:06 +0200

    Added migration

---
+++ src/Images/Images.Infrastructure/Migrations/20231127155854_AddImageToUserInfo.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BuildingMarket.Images.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageToUserInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "security");
+
+            migrationBuilder.CreateTable(
+                name: "AdditionalData",
+                schema: "security",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "character varying", maxLength: 255, nullable: true),
+                    last_name = table.Column<string>(type: "character varying", maxLength: 255, nullable: true),
+                    phone_number = table.Column<string>(type: "character varying", maxLength: 15, nullable: true),
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    image_url = table.Column<string>(type: "character varying", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdditionalData", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdditionalData",
+                schema: "security");
+        }
+    }
+}

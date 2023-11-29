c285b5879b8475d4af2d5b1d13b8aa4327c98d17
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-11-29 16:14:19 +0200

    Added AdditionalData changes to Properties microservice.

--- src/Properties/Properties.Infrastructure/Persistence/PropertiesContext.cs
+++ src/Properties/Properties.Infrastructure/Persistence/PropertiesContext.cs
@@ -282,6 +282,10 @@ namespace BuildingMarket.Properties.Infrastructure.Persistence
                 entity.Property(addData => addData.UserId)
                     .HasColumnName("user_id")
                     .IsRequired(true);
+
+                entity.Property(addData => addData.ImageURL)
+                    .HasColumnName("image_url")
+                    .HasColumnType("text");
             });
 
             modelBuilder.Entity<Image>(entity =>

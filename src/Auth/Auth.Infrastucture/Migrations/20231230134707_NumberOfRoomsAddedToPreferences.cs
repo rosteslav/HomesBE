39acab8fc6cb87b9142c19a67b97ffe7cd6c424f
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-12-30 15:48:32 +0200

    Added migration.

---
+++ src/Auth/Auth.Infrastucture/Migrations/20231230134707_NumberOfRoomsAddedToPreferences.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingMarket.Auth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NumberOfRoomsAddedToPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "number_of_rooms",
+                schema: "security",
+                table: "Preferences",
+                type: "character varying",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "number_of_rooms",
+                schema: "security",
+                table: "Preferences");
+        }
+    }
+}

934442d2a15902e7d59cfb67016554ee65b4a4cc
Petar Belchev <petar.belchev@outlook.com>
2024-01-08 12:50:13 +0200

    Calculate based on priceHigherEnd

--- src/Properties/Properties.Infrastructure/Repositories/RecommendationRepository.cs
+++ src/Properties/Properties.Infrastructure/Repositories/RecommendationRepository.cs
@@ -35,6 +35,8 @@ namespace BuildingMarket.Properties.Infrastructure.Repositories
             { BuildingTypes.Panel, new() { BuildingTypes.Brick, BuildingTypes.EPK } },
         };
 
+        private IEnumerable<(int Id, decimal Price)> _orderedProperties;
+
         public async Task<IEnumerable<int>> GetRecommended(BuyerPreferencesRedisModel preferences, CancellationToken cancellationToken)
         {
             _logger.LogInformation("DB get recommended properties");
@@ -44,8 +46,10 @@ namespace BuildingMarket.Properties.Infrastructure.Repositories
                 var properties = await _lazyProperties.Value;
                 var neighbourhoodsRating = await _lazyNeighbourhoodsRating.Value;
 
+                var gradesByPrice = CalculateGradesByPrice(preferences.PriceHigherEnd, await GetOrderedProperties());
+
                 var recommended = properties
-                    .Select(p => new { Id = p.Key, Grade = GradeProperty(p.Value, preferences) })
+                    .Select(p => new { Id = p.Key, Grade = GradeProperty(p.Value, preferences) + gradesByPrice[p.Key] })
                     .OrderByDescending(p => p.Grade)
                     .Take(RecommendedCount)
                     .Select(p => p.Id);
@@ -95,6 +99,105 @@ namespace BuildingMarket.Properties.Infrastructure.Repositories
             return buildingTypes.Contains(propertyBType) ? 10
                 : _nextToBuildingTypes[propertyBType].Overlaps(buildingTypes) ? 5
                 : 0;
+        }
+
+        private IDictionary<int, int> CalculateGradesByPrice(decimal priceHigherEnd, IEnumerable<(int Id, decimal Price)> orderedProperties)
+        {
+            var grades = orderedProperties.Select(p => (p.Id, default(int))).ToDictionary();
+
+            // Grading cheapest
+            var cheapest = orderedProperties.FirstOrDefault();
+            if (cheapest.Equals(default))
+                return grades;
+            grades[cheapest.Id] = 5;
+
+            // Grading closest cheaper price
+            int left = 0;
+            int right = orderedProperties.Count() - 1;
+            int closestCheaperIndex = FindClosestCheaperPropertyIndex(left, right, priceHigherEnd, orderedProperties);
+            var closestCheaper = closestCheaperIndex != -1 ? orderedProperties.ElementAt(closestCheaperIndex) : default;
+            if (!closestCheaper.Equals(default))
+                grades[closestCheaper.Id] = 10;
+
+            // Grading lower price
+            int grade = 0;
+            decimal gap = closestCheaper.Price - cheapest.Price;
+            decimal partPriceRange = gap / 4;
+            if (gap > 0 && partPriceRange > 0)
+            {
+                grade = 6;
+                left = 0;
+                right = closestCheaperIndex - 1;
+                for (decimal maxPrice = cheapest.Price + partPriceRange; maxPrice <= closestCheaper.Price; maxPrice += partPriceRange)
+                {
+                    int index = FindClosestCheaperPropertyIndex(left, right, maxPrice, orderedProperties);
+                    if (index != -1)
+                    {
+                        var id = orderedProperties.ElementAt(index).Id;
+                        grades[id] = grade;
+                        left = index;
+                    }
+
+                    grade++;
+                }
+            }
+
+            var mostExpensive = orderedProperties.LastOrDefault();
+            if (mostExpensive.Equals(closestCheaper))
+                return grades;
+
+            // Grading higher price
+            gap = mostExpensive.Price - priceHigherEnd;
+            partPriceRange = gap / 5;
+            grade = 0;
+            left = closestCheaperIndex + 1;
+            right = orderedProperties.Count() - 1;
+            for (decimal maxPrice = priceHigherEnd + partPriceRange; maxPrice <= mostExpensive.Price; maxPrice += partPriceRange)
+            {
+                var index = FindClosestCheaperPropertyIndex(left, right, maxPrice, orderedProperties);
+                if (index == -1)
+                    index = right;
+
+                var id = orderedProperties.ElementAt(index).Id;
+                grades[id] = grade++;
+                left = index;
+            }
+
+            return grades;
+        }
+
+        private int FindClosestCheaperPropertyIndex(int left, int right, decimal priceTarget, IEnumerable<(int Id, decimal Price)> properties)
+        {
+            while (left + 1 < right)
+            {
+                int mid = (right + left) / 2;
+                var midPrice = properties.ElementAt(mid).Price;
+                var nextToMidPrice = properties.ElementAt(mid + 1).Price;
+
+                if (midPrice <= priceTarget)
+                {
+                    if (nextToMidPrice > priceTarget)
+                        return mid;
+                    else
+                        left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return properties.ElementAt(right).Price <= priceTarget ? right
+                : properties.ElementAt(left).Price <= priceTarget ? left
+                : -1;
+        }
+
+        private async Task<IEnumerable<(int Id, decimal Price)>> GetOrderedProperties()
+        {
+            if (_orderedProperties == default)
+                _orderedProperties = (await _lazyProperties.Value).Select(p => (p.Key, p.Value.Price)).OrderBy(p => p.Price).ToArray();
+
+            return _orderedProperties;
         }
     }
 }
